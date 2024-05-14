c9547cd77cf10f537d10eab0deccdf9665c11d7b
fourbarman <maks.java@yandex.ru>
2024-05-14 12:51:44 +0400

    Sample testing bean added.

--- notification/src/main/java/ru/fourbarman/notification/NotificationApplication.java
+++ notification/src/main/java/ru/fourbarman/notification/NotificationApplication.java
@@ -1,11 +1,20 @@
 package ru.fourbarman.notification;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import ru.fourbarman.amqp.RabbitMQMessageProducer;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "ru.fourbarman.notification",
+                "ru.fourbarman.amqp"
+        }
+
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "ru.fourbarman.clients"
@@ -14,4 +23,16 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+        return args -> {
+            producer.publish(
+                    new Person("Bill", 18),
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
+    }
+
+    record Person(String name, int age){}
 }
